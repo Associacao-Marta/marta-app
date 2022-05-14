ba81a0823695ea767282be4d12777c40fec733b3
Sami Nagahama <sami.nagahama@ifood.com.br>
2022-05-14 12:44:27 -0300

    Versões capengas do D3, D4, D5 e F1

--- src/telas/D4.jsx
+++ src/telas/D4.jsx
@@ -1,7 +1,38 @@
 import React from "react";
+import { Typography } from "@mui/material";
+import { Button, Stack, Grid, TextField } from "@mui/material";
+import BotaoVoltarSuperior from "../components/BotaoVoltarSuperior";
+import BotaoDoPanico from "../components/BotaoDoPanico";
+import MenuInferior from "../components/MenuInferior";
 
 const D4 = () => {
-  return;
+  return (
+    <Typography>
+      <Grid container>
+        <BotaoVoltarSuperior />
+        <Grid item xs={8}>
+          <TextField
+            label="Protocolo"
+            variant="outlined"
+            style={{
+              width: 300,
+            }}
+          />
+        </Grid>
+        <Grid>
+          <overline>
+            Para confirmar data e horário do seu atendimento, basta inserir o
+            número de protocolo que te enviamos por WhatsApp.
+          </overline>
+        </Grid>
+
+        <Grid item xs={12}>
+          <BotaoDoPanico />
+          <MenuInferior />
+        </Grid>
+      </Grid>
+    </Typography>
+  );
 };
 
 export default D4;
--- src/telas/D5.jsx
+++ src/telas/D5.jsx
@@ -1,7 +1,32 @@
 import React from "react";
+import { Button, Typography } from "@mui/material";
+import { Card, Grid } from "@mui/material";
 
 const D5 = () => {
-  return;
+  return (
+    <Typography>
+      <Card>
+        <Grid container>
+          <Grid>
+            <subtitle1 style={{ fontSize: 16, fontWeight: 600 }}>
+              Atendimento agendado
+            </subtitle1>
+          </Grid>
+          <Grid>
+            <body2>Olá! Muito obrigada por agendar um horário conosco!</body2>
+          </Grid>
+          <Grid>
+            <body2>
+              Estamos ansiosas para te auxiliar da melhor forma possível!{" "}
+            </body2>
+          </Grid>
+          <Grid item xs={12}>
+            <Button variant="contained">OK</Button>
+          </Grid>
+        </Grid>
+      </Card>
+    </Typography>
+  );
 };
 
 export default D5;
