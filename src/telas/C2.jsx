cb8efdd964dd05932e08d4a6a8b65324ca6099fd
Sami Nagahama <sami.nagahama@ifood.com.br>
2022-05-14 12:12:52 -0300

    Versões capengas do C2, D1 e D2

--- src/telas/C2.jsx
+++ src/telas/C2.jsx
@@ -1,7 +1,79 @@
 import React from "react";
+import { Typography } from "@mui/material";
+import { Card, Chip, Grid, Stack } from "@mui/material";
+import CircleIcon from "@mui/icons-material/Circle";
+import BotaoVoltarSuperior from "../components/BotaoVoltarSuperior";
+import BotaoDoPanico from "../components/BotaoDoPanico";
+import MenuInferior from "../components/MenuInferior";
+
+//  procurar barrinha pra antes do título do artigo
 
 const C2 = () => {
-  return;
+  return (
+    <Typography>
+      <Grid container>
+        <BotaoVoltarSuperior />
+        <Grid item xs={12}>
+          <CircleIcon sx={{ fontSize: "100px" }} />
+        </Grid>
+        <Grid item xs={8}>
+          <h2>Título do Artigo</h2>
+        </Grid>
+        <Grid item xs={12} sx={{ fontSize: "11px" }}>
+          <overline>
+            Nulla nisl integer egestas tellus. Hac id facilisis sit quis sit
+            pellentesque pulvinar. Faucibus cras.
+          </overline>
+        </Grid>
+        <Grid item xs={6}>
+          <Chip label="Tema" style={{ margin: 5 }} />
+          <Chip label="Filtros" style={{ margin: 5 }} />
+        </Grid>
+        <Grid>
+          <body2>
+            Suspendisse nisi elit lobortis tristique. In facilisis consequat
+            hendrerit viverra sagittis diam lorem consequat lobortis. Felis,
+            augue eu massa nisl, sed est purus sollicitudin.
+          </body2>
+        </Grid>
+        <Grid>
+          <body2>
+            Nec viverra in consectetur aliquet eu donec adipiscing risus sed.
+            Pulvinar bibendum facilisis pellentesque eu morbi mattis condimentum
+            imperdiet.
+          </body2>
+        </Grid>
+        <Grid>
+          <body2>
+            Blandit eget tempor egestas pulvinar faucibus tristique. Diam sit
+            quam blandit tristique sed. Leo euismod fames tellus porttitor ut
+            lacus fames eget. Nulla adipiscing pharetra et fermentum aliquet.
+            Amet diam.
+          </body2>
+        </Grid>
+        <Grid>
+          <subtitle1 style={{ fontSize: 16, fontWeight: 600 }}>
+            Talvez você também goste
+          </subtitle1>
+        </Grid>
+        <Grid>
+          <Stack direction="row" spacing={4}>
+            <Card style={{ padding: 25 }}>
+              <h4>Artigo</h4>
+            </Card>
+            <Card style={{ padding: 25 }}>
+              <h4>Artigo</h4>
+            </Card>
+            <Card style={{ padding: 25 }}>
+              <h4>Artigo</h4>
+            </Card>
+          </Stack>
+        </Grid>
+      </Grid>
+      <BotaoDoPanico />
+      <MenuInferior />
+    </Typography>
+  );
 };
 
 export default C2;
--- src/telas/D2.jsx
+++ src/telas/D2.jsx
@@ -1,7 +1,78 @@
 import React from "react";
+import { Typography } from "@mui/material";
+import { Button, Stack, Grid, TextField } from "@mui/material";
+import BotaoVoltarSuperior from "../components/BotaoVoltarSuperior";
+import BotaoDoPanico from "../components/BotaoDoPanico";
+import MenuInferior from "../components/MenuInferior";
 
 const D2 = () => {
-  return;
+  return (
+    <Typography>
+      <Grid container>
+        <BotaoVoltarSuperior />
+        <Grid item xs={12}>
+          <subtitle1 style={{ fontSize: 16, fontWeight: 600 }}>
+            Nos conte um pouco sobre você
+          </subtitle1>
+        </Grid>
+        <Grid item xs={8}>
+          <Stack direction="column">
+            <TextField
+              label="Nome Completo"
+              variant="outlined"
+              style={{
+                width: 300,
+              }}
+            />
+            <TextField
+              label="Telefone"
+              variant="outlined"
+              style={{
+                width: 300,
+              }}
+            />
+            <TextField
+              label="Descrição"
+              variant="outlined"
+              multiline
+              rows={4}
+              style={{
+                width: 300,
+              }}
+            />
+          </Stack>
+        </Grid>
+        <Grid item xs={12}>
+          <subtitle1 style={{ fontSize: 16, fontWeight: 600 }}>
+            Com que profissional gostaria de falar?
+          </subtitle1>
+        </Grid>
+        <Grid item xs={12}>
+          <Stack direction="row" spacing={12}>
+            <Button variant="contained">psicóloga</Button>
+            <Button variant="contained">advogada</Button>
+          </Stack>
+        </Grid>
+        <Grid item xs={12}>
+          <subtitle1 style={{ fontSize: 16, fontWeight: 600 }}>
+            Quando gostaria de ser atendida?
+          </subtitle1>
+        </Grid>
+        <Grid item xs={12}>
+          calendario
+        </Grid>
+        <Grid item xs={12}>
+          <Button variant="contained" sx={{ margin: 5 }}>
+            Enviar
+          </Button>
+        </Grid>
+        <Grid item xs={12}>
+          <BotaoDoPanico />
+          <MenuInferior />
+        </Grid>
+      </Grid>
+    </Typography>
+  );
 };
 
 export default D2;
