15353c7f98a04aed9842ee7ce2f898ba1f00278b
Sami Nagahama <sami.nagahama@ifood.com.br>
2022-05-16 11:17:52 -0300

    .

--- src/App.js
+++ src/App.js
@@ -1,22 +1,15 @@
 import React from "react";
 import { Typography } from "@mui/material";
-
-import { ReactDOM } from "react";
-
 import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
 
 import A1 from "./telas/A1";
-import B1 from "./telas/B1.jsx";
-import C1 from "./telas/C1.jsx";
-import C2 from "./telas/C2.jsx";
-import D1 from "./telas/D1.jsx";
-import D2 from "./telas/D2.jsx";
-import D3 from "./telas/D3";
+import B1 from "./telas/B1";
+import C1 from "./telas/C1";
+import C2 from "./telas/C2";
+import D1 from "./telas/D1";
+import D2 from "./telas/D2";
 import D4 from "./telas/D4";
-import D5 from "./telas/D5";
 import E1 from "./telas/E1";
-import E2 from "./telas/E1";
-import F1 from "./telas/F1";
 
 function App() {
   return (
