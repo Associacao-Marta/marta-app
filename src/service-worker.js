4f9e9cfb3e1af022368f771fee8ceca0be4ec5da
Sami Nagahama <nagahamasami@gmail.com>
2022-07-09 17:00:52 -0300

    Prompt A2HS no service worker e index.html

--- src/service-worker.js
+++ src/service-worker.js
@@ -61,6 +61,14 @@ registerRoute(
   })
 );
 
+self.addEventListener('fetch', function(event) {
+  event.respondWith(
+      caches.match(event.request).then(function(response) {
+          return response || fetch(event.request);
+      })
+  );
+});
+
 // This allows the web app to trigger skipWaiting via
 // registration.waiting.postMessage({type: 'SKIP_WAITING'})
 self.addEventListener('message', (event) => {
