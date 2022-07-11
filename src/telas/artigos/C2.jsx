14a32ca1dd0f221a9e037cbf3a9c6d99966eee70
Lucas <emanuelx.lucas@gmail.com>
2022-07-11 16:19:13 -0300

    correcao bug artigo C2

--- src/telas/artigos/C2.jsx
+++ src/telas/artigos/C2.jsx
@@ -26,31 +26,43 @@ const C2 = () => {
           <div className="content">
             <LeiaMais className="leiaMais">
               <p>
-                Uma Associação composta apenas por mulheres, que empatiza com
-                a causa das vítimas da violência estrutural da sociedade, atuando na sua prevenção e conscientização.
+                Uma Associação composta apenas por mulheres, que empatiza com a
+                causa das vítimas da violência estrutural da sociedade, atuando
+                na sua prevenção e conscientização.
               </p>
               <p>
                 A Associação Marta leva este nome em homenagem a uma mulher
-                anônima que, assim como muitas, já sofreu com a violência doméstica,
-                mas que tem uma história inspiradora de superação.
+                anônima que, assim como muitas, já sofreu com a violência
+                doméstica, mas que tem uma história inspiradora de superação.
               </p>
               <b>O que fazemos?</b>
-              <p>               
-                Além de realizarmos palestras virtuais e presenciais, temos forte
-                presença nas redes sociais com o objetivo de conscientizar e educar os
-                estudantes e a comunidade, a fim de que se tornem cidadãos mais
-                responsáveis, sensíveis aos temas relacionados à causa da Associação.
+              <p>
+                Além de realizarmos palestras virtuais e presenciais, temos
+                forte presença nas redes sociais com o objetivo de conscientizar
+                e educar os estudantes e a comunidade, a fim de que se tornem
+                cidadãos mais responsáveis, sensíveis aos temas relacionados à
+                causa da Associação.
               </p>
               <p>
-              Também promovemos projetos de assistência jurídica e psicológica gratuitas para vítimas de violência contra a mulher.
+                Também promovemos projetos de assistência jurídica e psicológica
+                gratuitas para vítimas de violência contra a mulher.
               </p>
               <b>Por que fazemos?</b>
               <p>
-                Acreditamos na importância da conscientização por meio de palestras e
-                pelas redes sociais, tendo em vista que muitas mulheres e adolescentes
-                não sabem identificar as formas de violência, não estando preparadas
-                para se proteger delas. Ainda, este método nos possibilita capacitar o
-                público-alvo para perceber, se sensibilizar e dar suporte quando se depara com uma situação de violência.
+                Acreditamos na importância da conscientização por meio de
+                palestras e pelas redes sociais, tendo em vista que muitas
+                mulheres e adolescentes não sabem identificar as formas de
+                violência, não estando preparadas para se proteger delas. Ainda,
+                este método nos possibilita capacitar o público-alvo para
+                perceber, se sensibilizar e dar suporte quando se depara com uma
+                situação de violência.
+              </p>
+              <b>Contatos</b>
+              <p>
+                Instagram: <a href="https://www.instagram.com/associacaomarta/">https://www.instagram.com/associacaomarta/</a> <br/>
+                Facebook: <a href="https://www.facebook.com/AssociacaoMarta/">https://www.facebook.com/AssociacaoMarta/</a> <br/>
+                E-mail: <a href="mailto:contato.associacaomarta@gmail.com">contato.associacaomarta@gmail.com</a> <br/>
+                Whatsapp: <a href="tel:+558599612-0974">(85) 99612-0974</a>
               </p>
             </LeiaMais>
           </div>
